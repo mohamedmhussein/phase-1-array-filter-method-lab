90d17fbc82b9de70b6bf3282c451864f22b50cc3
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-04-02 19:08:43 -0400

    filter array

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+
+function findMatching(array, driver){
+    return array.filter(item => item.toUpperCase() === driver.toUpperCase())
+}
+
+function fuzzyMatch(array, driver){
+     return array.filter(item => {
+        let i = 0;
+        let match = true;
+        for (const letter of driver){
+          //console.log(i, match)
+            if (letter !== item[i]){
+              i++
+              match = false;
+            
+          }
+          i++
+          //console.log(i, match)
+            
+        }
+      return match  
+       })
+}
+
+function matchName(drivers, property){
+return drivers.filter(item =>  item.name === property)
+
+}
+//let de = fuzzyMatch(["Mohamed", "Mosad", "Moaz", "Sally"], "Sa")
+//console.log(de)
